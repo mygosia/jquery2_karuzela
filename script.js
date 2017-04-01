c08fcf458ea238af34f00deaaa811c567dc044ef
mygosia <mygosia@gmail.com>
2017-04-01 19:37:56 +0200

    prawiedziala2

--- script.js
+++ script.js
@@ -1,28 +1,18 @@
 $(function(){
 	var carouselList = $(".carousel ul");
 
-	setInterval(function()  {
-		$(".carousel ul").animate({
-			marginLeft:-400}
-			, 500)}
-	);
+	function slideImg() {
+		carouselList.animate({
+			marginLeft:-526})		
+	};
+
+	setInterval(slideImg, 1500, moveFirstSlide);
 	
-
-	//function interval () {
-	//	setInterval(function(){ alert("Hello"); }, 3000);
-	//};
-	//$("body").click(interval);
-
-
-	//$(".carousel ul").setInterval(function(){ alert("Hello"); }, 3000);
-	/*
-	function changeSlide () {
-		carouselList.animate({marginleft: '-=526px'}, 500, moveFirstSlide);
-	}
 	function moveFirstSlide () {
 		var firstItem = carouselList.find("li:first");
 		var lastItem = carouselList.find("li:last");
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft:0});
-	}*/
+	};
+
 });
