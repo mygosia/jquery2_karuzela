c8382dc45125022ada277760c3e028cc7f8db7fa
mygosia <mygosia@gmail.com>
2017-04-03 22:07:26 +0200

    dzialaja od II klikniecia

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 $(function(){
 	var carouselList = $(".carousel ul");
-
-	function slideImg() {
-		carouselList.animate({
-			marginLeft:-526}, 500, moveFirstSlide)		
-	};
-
-	setInterval(slideImg, 1500);
 	
+
+	function slideImgNext() {
+		carouselList.animate({
+			marginLeft:-526}, 1500, moveFirstSlide)		
+	};
 	function moveFirstSlide () {
 		var firstItem = carouselList.find("li:first");
 		var lastItem = carouselList.find("li:last");
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft:0});
 	};
-
+	function slideImgBack() {		
+		carouselList.animate({
+			marginLeft: 0}, 1500, moveLastSlide)		
+	};
+	function moveLastSlide () {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		firstItem.before(lastItem);
+		carouselList.css({marginLeft: -526});
+	};
+	$(".nextBtn").click(
+		slideImgNext		
+	);
+	$(".backBtn").click(
+		moveLastSlide,
+		slideImgBack
+	);
 });
