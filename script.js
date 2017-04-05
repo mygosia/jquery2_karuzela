dd9b05ddeb53ae503a35fb3b303574817b76b1c9
mygosia <mygosia@gmail.com>
2017-04-05 18:37:48 +0200

    zmienilam - nadal niedobrze

--- script.js
+++ script.js
@@ -2,31 +2,29 @@ $(function(){
 	var carouselList = $(".carousel ul");
 	
 
-	function slideImgNext() {
-		carouselList.animate({
-			marginLeft:-526}, 1500, moveFirstSlide)		
-	};
 	function moveFirstSlide () {
 		var firstItem = carouselList.find("li:first");
 		var lastItem = carouselList.find("li:last");
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft:0});
 	};
-	function slideImgBack() {		
+	function slideImgNext() {
 		carouselList.animate({
-			marginLeft: 0}, 1500, moveLastSlide)		
+			marginLeft:-526}, 1500, moveFirstSlide)		
 	};
+	
 	function moveLastSlide () {
 		var firstItem = carouselList.find("li:first");
 		var lastItem = carouselList.find("li:last");
 		firstItem.before(lastItem);
 		carouselList.css({marginLeft: -526});
 	};
-	$(".nextBtn").click(
-		slideImgNext		
-	);
-	$(".backBtn").click(
-		moveLastSlide,
-		slideImgBack
-	);
+
+	function slideImgBack() {
+		moveLastSlide,		
+		carouselList.animate({
+			marginLeft: 0}, 1500, moveLastSlide)		
+	};
+	$(".nextBtn").on("click", slideImgNext);
+	$(".backBtn").on("click", slideImgBack);
 });
